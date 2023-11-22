639d7b6269d6a0615abded05d2f00183e3ed141b
kevin collier <collier.kevin.m@gmail.com>
2023-11-22 22:15:40 +0900

    added a button for deleting a todo in TodoItem component

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,12 +2,14 @@ import { Todo } from '../types/Todo'; // Import the Todo type
 
 type TodoItemProps = {
   todo: Todo;
+  onDelete: (todoId: string) => void;
 };
 
-const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
+const TodoItem: React.FC<TodoItemProps> = ({ todo, onDelete }) => {
   return (
     <div className="bg-white p-2 mb-2 rounded shadow-sm">
       <p className="text-gray-700">{todo.title}</p>
+      <button onClick={() => onDelete(todo._id)}> - </button>
     </div>
   );
 };
