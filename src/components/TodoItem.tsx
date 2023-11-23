44eeb82484870fb08b96110adf888e54461f3a14
kevin collier <collier.kevin.m@gmail.com>
2023-11-23 15:48:40 +0900

    replaced trashcan with an x

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -7,12 +7,11 @@ type TodoItemProps = {
 
 const TodoItem: React.FC<TodoItemProps> = ({ todo, onDelete }) => {
   return (
-    <div className="bg-white p-2 mb-2 rounded shadow-sm flex justify-between items-center">
+    <div className="bg-white p-2 mb-2 rounded shadow-md flex justify-between items-center">
       <p className="text-gray-700 flex-1">{todo.title}</p>
       <button onClick={() => onDelete(todo._id)} className="text-gray-400 hover:text-gray-500 ml-10">
         <svg className="w-5 h-5" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
           <path d="M6 18L18 6M6 6l12 12"></path>
-          <path d="M19 6h-2.5L15 4H9l-1.5 2H5v2h14V6zM6 8v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V8H6z"></path>
         </svg>
       </button>
     </div>
