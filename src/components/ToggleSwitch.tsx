f9137e667f3b2c13753f219365d81c6dc62ae9e9
kevin collier <collier.kevin.m@gmail.com>
2023-11-23 15:32:25 +0900

    added a transition effect with hover

--- src/components/ToggleSwitch.tsx
+++ src/components/ToggleSwitch.tsx
@@ -7,13 +7,13 @@ const ToggleSwitch: React.FC<ToggleSwitchProps> = ({ isToday, setTodayView }) =>
   return (
     <div className="flex justify-center space-x-2 mb-4">
       <button
-        className={`px-4 py-2 rounded-lg ${isToday ? 'bg-blue-500 text-white' : 'bg-transparent border border-gray-300'}`}
+        className={`px-4 py-2 rounded-lg transition-colors duration-300 ${isToday ? 'bg-blue-500 text-white' : 'bg-transparent border border-gray-300 hover:bg-blue-100'}`}
         onClick={() => setTodayView(false)}
       >
         My To Do List
       </button>
       <button
-        className={`px-4 py-2 rounded-lg ${!isToday ? 'bg-blue-500 text-white' : 'bg-transparent border border-gray-300'}`}
+        className={`px-4 py-2 rounded-lg transition-colors duration-300 ${!isToday ? 'bg-blue-500 text-white' : 'bg-transparent border border-gray-300 hover:bg-blue-100'}`}
         onClick={() => setTodayView(true)}
       >
         Today
@@ -21,5 +21,6 @@ const ToggleSwitch: React.FC<ToggleSwitchProps> = ({ isToday, setTodayView }) =>
     </div>
   );
 };
+
 
 export default ToggleSwitch;
